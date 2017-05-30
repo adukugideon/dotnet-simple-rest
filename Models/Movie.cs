5b3f3c25d62db1e4109ed89bf63f717474bd97cd
Gideon Aduku <gideonaduku@gmail.com>
2017-05-30 07:22:41 +0000

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+sing System;
+using System.ComponentModel.DataAnnotations;
+
+namespace movieAPI
+{
+    public class Book
+    {
+   
+        public int Id { get; set; }
+       
+        public string title { get; set; }
+
+        public string year { get; set; }
+        public string genre { get; set; }
+        public string rating { get; set; }
+        public bool viewed { get; set; }
+        }
+}
